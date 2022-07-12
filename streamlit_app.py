83aedc923f089c7173378fba599f79f753f947ce
brittso <109121455+brittso@users.noreply.github.com>
2022-07-13 08:22:26 +1200

    Update streamlit_app.py
    adding pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,9 @@ streamlit.header('this')
 
 streamlit.text('🤞 this')
 streamlit.text('🍞 this')
+
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+strea lit.dataframe('my fruit list')
